03ea6d42f4be08a09c5b451f9944d5ab81dc41cd
skydrive <2422978538@qq.com>
2025-03-30 18:44:27 +0800

    update

---
+++ skydrive.py
@@ -0,0 +1,22 @@
+a=1
+b=2
+c=10086
+print("hello world",a,b,c)
+while c>10000:
+    if a == 1:
+        print(c)
+        c=c-1
+    else:
+        print(b)
+
+
+
+for i in range(10):
+    print(i)
+
+test_list=[]
+list1=[1,2,3]
+list2=['a',b,b]
+test_list.append(list1)
+test_list.append(list2)
+print(test_list)
