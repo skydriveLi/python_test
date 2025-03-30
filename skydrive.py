8358e54769a2be5d10ac1bd31ee8ec3d49dd24f7
IMJX6666 <happyjianxin@163.com>
2025-03-30 21:47:35 +1100

    abc

--- skydrive.py
+++ skydrive.py
@@ -2,12 +2,12 @@ a=1
 b=2
 c=10086
 print("hello world",a,b,c)
-while c>10000:
-    if a == 1:
-        print(c)
-        c=c-1
-    else:
-        print(b)
+# while c>10000:
+#     if a == 1:
+#         print(c)
+#         c=c-1
+#     else:
+#         print(b)
 
 
 
