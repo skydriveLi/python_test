4a1d9ef9e5d6a328418e56199a6878de5f04314f
skydrive <2422978538@qq.com>
2025-03-30 19:27:37 +0800

    24229

--- engine.py
+++ engine.py
@@ -6,14 +6,29 @@ u=0.1
 v=1
 dt=0.01
 s=0
-turn_off=True
-while turn_off:
+all_time=0
+turn_off=False
+while ~turn_off:
     f=0
     if v!=0:
         f=m*g*u*abs(v)/v*-1
     a=f/m
     v0=v
     v=v+a*dt
-    s=s+ v0*dt + 0.5*a*dt*dt
-    print(f"\r{s} {v} {a}",end="")
-    time.sleep(dt)
+    dt_=dt
+
+    if v*v0<0:
+        v=0
+        dt_=abs(v0/a)
+
+    s=s+ v0*dt_ + 0.5*a*dt*dt_
+    if dt_ > 0:
+        time.sleep(dt_)
+        all_time=all_time+dt_
+    else:
+        time.time(dt)
+        all_time=all_time+dt
+    print(f"\r time:{format(all_time,'4.4f')} {s} {v} {a}",end="")
+
+    if all_time>10:
+        turn_off=True
